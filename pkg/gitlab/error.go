468e89f6e0c50a0565bd620d4450abd9bfe75118
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2024-11-21 21:05:49 +0100

    docs: Add comments to ErrorMessage struct for clarity on API error handling

--- pkg/gitlab/error.go
+++ pkg/gitlab/error.go
@@ -1,5 +1,7 @@
 package gitlab
 
+// ErrorMessage is a struct that contains an error message
+// It is returned by the Gitlab API when an error occurs
 type ErrorMessage struct {
 	Message string `json:"message"`
 }
